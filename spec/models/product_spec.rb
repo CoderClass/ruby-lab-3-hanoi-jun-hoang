aa69f0fa20ffe97d4e15ca80abc9cabe221c7cd9
jundalisay <jundalisay@yahoo.com>
2016-10-25 20:40:01 +0700

    milestone 4.1

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe ".alphabetical" do
+
+    # product1 = FactoryGirl.create(:product, name: "Brad's Hata")
+    # product2 = FactoryGirl.create(:product, name: "Brad's Hatb")
+    # product3 = FactoryGirl.create(:product, name: "Brad's Hatc")
+
+    it "asdf" do
+      b = Product.create! name: "adsf1"    
+      a = Product.create! name: "adsf"    
+      c = Product.create! name: "adsf2"    
+      expect(Product.alphabetical).to eq [a, b, c]
+    end
+  end
 end
